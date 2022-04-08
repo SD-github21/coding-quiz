7cb04fd7dc026245e2b7477429f623b2d94a1a9c
SD-github21 <sapandonde.gmail.com>
2022-04-07 23:39:33 -0500

    added first 2 quiz questions in array

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var questions = [
+    { id: 0,
+    question: "Commonly used data types DO Not include the following: ",
+    answer: [{ choice1: "1. strings", correctAnswer: false},
+             { choice2: "2. booleans", correctAnswer: false},
+             { choice3: "3. alerts", correctAnswer: true},
+             { choice4: "4. numbers", correctAnswer: false}
+            ]
+    },
+    { id: 1,
+    question: "The condition in an if/else statement is enclosed with______________. ",
+    answer: [{ choice1: "1. quotes", correctAnswer: false},
+            { choice2: "2. curly braces", correctAnswer: true},
+            { choice3: "3. parantheses", correctAnswer: false},
+            { choice4: "4. square brackets", correctAnswer: false}
+            ]
+    
+    }
+
+
+}]
